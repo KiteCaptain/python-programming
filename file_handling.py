905875a62131f09bc7acb39f9c41f5038923732c
KiteCaptain <97241357+KiteCaptain@users.noreply.github.com>
2022-08-16 06:05:10 +0300

    file handling project

--- file_handling.py
+++ file_handling.py
@@ -1,3 +1,9 @@
 with open('testfile.txt', 'a') as file:
     file.write("\nI am happy \ntoday!")
     file.writelines(["\nFeels good to be Okay","\nI freaking love programming"])
+
+import random
+f = open("petnames.txt", "r")
+f_content = f.read()
+f_content_list = f_content.split("\n")
+print(random.choice(f_content_list))
