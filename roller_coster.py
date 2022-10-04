41c5ab02eee22d17ffe5269f00b92b904622c6fb
KiteCaptain <97241357+KiteCaptain@users.noreply.github.com>
2022-10-04 18:41:21 +0300

    refined

--- roller_coster.py
+++ roller_coster.py
@@ -6,9 +6,15 @@
 def payment_verification(name: str, height_in_cm: float, age_in_yrs: int) -> str:
     if height_in_cm < 120:
         print(f"Dear {name}, you have not attained the minimum height, please step down")
-    elif height_in_cm >= 120 and age_in_yrs >= 18:
-        print(f"Dear {name}, welcome onboard the your ticket price is Ksh1200")    
+    elif height_in_cm >= 120 and age_in_yrs < 18:
+        print(f"Dear {name}, welcome onboard your ticket price is Ksh700")    
     else:
-        print(f"Dear {name}, welcome onboard the your ticket price is Ksh1200")
+        print(f"Dear {name}, welcome onboard your ticket price is Ksh1200")
 
-payment_verification("Kite", 130, 19)
+payment_verification("Kite", 130, 19) # -> Dear Kite, welcome onboard your ticket price is Ksh1200
+
+payment_verification("Joe", 110, 19) # -> Dear Joe, you have not attained the minimum height, please step down
+
+payment_verification("Ryan", 120, 17) # -> Dear Ryan, welcome onboard your ticket price is Ksh700
+
+payment_verification("Jane", 113, 15) # -> Dear Jane, you have not attained the minimum height, please step down
