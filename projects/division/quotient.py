ec29af9400f40f97cd5a86f81b1b86b8e84b9f7b
KiteCaptain <97241357+KiteCaptain@users.noreply.github.com>
2022-10-11 17:07:34 +0300

    changed inputs

--- projects/division/quotient.py
+++ projects/division/quotient.py
@@ -1,6 +1,6 @@
 # The quotient of 14 with 3 is 4 with a remainder of 2
 
-def division(dividend, divisor):
+def division(dividend: float, divisor: float) -> str:
     quotient = dividend // divisor
     rem = dividend - (quotient * divisor)
     if rem == 0:
@@ -8,5 +8,6 @@ def division(dividend, divisor):
    
     return f"The quotient of {dividend} and {divisor} is: {quotient} and the remainder is: {rem}"
 
-print(division(20, 3)) # The quotient of 20 and 3 is: 6 and the remainder is: 2
-print(division(20, 4)) # The quotient of 20 and 4 is 5 with no remainder
+print(division(100, 3)) # The quotient of 100 and 3 is: 33 and the remainder is: 1
+
+print(division(50, 5)) # The quotient of 50 and 5 is 10 with no remainder
